c18f6ea5a06b13e737ae04ab2b6bda9cbae018fa
sumanta ghosh <84187999+sumantaGhosh24@users.noreply.github.com>
2026-04-28 09:20:53 +0530

    add neon database using prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
+import prisma from "@/lib/db";
 import ModeToggle from "@/components/mode-toggle";
 
 export const metadata = {
   title: "Home",
 };
 
-const Home = () => {
+const Home = async () => {
+  const users = await prisma.user.findMany();
+
   return (
     <div>
       <h1>Home</h1>
       <ModeToggle />
+      <p>{JSON.stringify(users)}</p>
     </div>
   );
 };
